1e3dbfb83189fef023b03c56d3598a25677b3e61
rockinthespecs <kaitlyn.sandor@gmail.com>
2017-01-10 19:00:49 -0500

    added stuport for has next

--- Deck.java
+++ Deck.java
@@ -1,4 +1,3 @@
-
 /**
  * Write a description of class Deck here.
  * 
@@ -7,36 +6,48 @@
  */
 public class Deck
 {
-    private final int NUMCARDS = 52;
-    private Card[] deck = new Card[NUMCARDS];
+    public final int NUMCARDS = 52;
+    private int counter = 0;
+    public Card[] deck = new Card[NUMCARDS];
     public Deck()
     {
-        for(int i =0; i < NUMCARDS; i++)
-        {
-            for(Card.FaceValue x : Card.FaceValue.values())
-                for(Card.Suit y : Card.Suit.values())
-                    deck[i] = new Card(x,y);
-        }
+        int counter = 0;
+        for(Card.FaceValue x : Card.FaceValue.values())
+            for(Card.Suit y : Card.Suit.values())
+            {deck[counter] = new Card(x,y);
+                counter++;}
     }
 
     public Card getNextCard()
     {
-        return deck[0];
+        if(!hasNext())
+                counter = 0;
+            counter++;
+            return deck[counter-1];
     }
 
     public void shuffle()
     {
-        for(int i = deck.length -1; i > 0; i--)
+        for(int i = 0; i < deck.length-1; i++)
         {
-            int index = (int)Math.random()*NUMCARDS;
-            Card a = deck[index];
-            deck[index] = deck[index+1];
-            deck[index+1] = a;
+            int index = (int)(Math.random()*(deck.length-1 - i)+ i);
+            Card a = deck[index+1];
+            deck[index+1] = deck[i];
+            deck[i] = a;
         }
-        public boolean hasNext()
-        {
-        }
-        public void dump()
-        {
-        }
+        counter = 0;
     }
+
+    public boolean hasNext()
+    {
+        if(counter < NUMCARDS)
+            return true;
+        return false;
+    }
+
+    public void dump()
+    {
+        for(Card x: deck)
+            System.out.println(x);
+    }
+}
